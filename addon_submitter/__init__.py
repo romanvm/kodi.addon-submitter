98fe145eef16320ddd806968ac4a7afdbd29f812
Roman Miroshnychenko <romanvm@yandex.ru>
2017-11-25 22:21:22 +0200

    Initial commit

---
+++ addon_submitter/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'addon_submitter.apps.AddonSubmitterAppConfig'
---
+++ addon_submitter/apps.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Author: Roman Miroshnychenko aka Roman V.M.
+# E-mail: roman1972@gmail.com
+
+from django.apps.config import AppConfig
+
+
+class AddonSubmitterAppConfig(AppConfig):
+    name = 'addon_submitter'
+    verbose_name = 'Kodi Addon Submitter'
