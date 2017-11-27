ecfc21aadc4e512304f5c54eb3ac330e9977d4bc
Roman Miroshnychenko <romanvm@yandex.ru>
2017-11-27 17:26:00 +0200

    Implement preparing a repository for GitHub pull request

--- addon_submitter/pull_request.py
+++ addon_submitter/pull_request.py
@@ -4,12 +4,38 @@
 """Functions for working with GitHub pull requests"""
 
 import os
+import logging
 import shutil
+import subprocess
 from django.conf import settings
-from .zip_file import ZippedAddon
+from addon_submitter.zip_file import ZippedAddon
 
 REPO_URL_MASK = 'https://{gh_token}@github.com/{user}/{repo}.git'
 GH_TOKEN = os.environ['GH_TOKEN']
+
+logger = logging.getLogger(__name__)
+if settings.DEBUG:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+logger.setLevel(level)
+
+
+def execute(args: list) -> None:
+    """
+    Execute a console command
+
+    :param args: command with arguments
+    :raises RuntimeError: if command returns non-0 code
+    """
+    call_string = ' '.join(args)
+    logging.debug('Executing: ' + call_string)
+    res = subprocess.call(args)
+    if res:
+        raise RuntimeError('Call {call} returned error code {res}!'.format(
+            call=call_string,
+            res=res
+        ))
 
 
 def create_addon_directory(workdir: str, zipaddon: ZippedAddon) -> None:
@@ -45,35 +71,54 @@ def prepare_pr_branch(repo: str, branch: str, workdir: str,
         user= settings.PROXY_USER,
         repo=repo
     )
-    os.system('git clone {0}'.format(repo_url))
+    execute(['git', 'clone', repo_url])
     os.chdir(repo)
-    os.system('git config user.name {0}'.format(settings.USER_NAME))
-    os.system('git config user.email {0}'.format(settings.USER_EMAIL))
-    os.system(
-        'git remote add upstream https://github.com/{user}/{repo}.git'.format(
-        user=settings.UPSTREAM_USER,
-        repo=repo
-    ))
-    os.system('git fetch upstrem')
-    os.system('git checkout -b {0} --track origin/{0}'.format(branch))
-    os.system('git merge upstream/{0}'.format(branch))
-    os.system('git branch -D {0}'.format(addon_id))
-    os.system('git checkout -b {0}'.format(addon_id))
+    execute(['git', 'config', 'user.name', settings.USER_NAME])
+    execute(['git', 'config', 'user.email', settings.USER_EMAIL])
+    execute(['git', 'remote', 'add', 'upstream',
+             'https://github.com/{user}/{repo}.git'.format(
+                 user=settings.UPSTREAM_USER,
+                 repo=repo
+             )])
+    execute(['git', 'fetch', 'upstream'])
+    execute(['git', 'checkout', '-b', branch, '--track',
+             'origin/{0}'.format(branch)])
+    execute(['git', 'merge', 'upstream/{0}'.format(branch)])
+    os.system('git branch -D ' + addon_id)
+    execute(['git', 'checkout', '-b', addon_id])
     shutil.rmtree(os.path.join(workdir, repo, addon_id), ignore_errors=True)
     shutil.copytree(os.path.join(workdir, addon_id),
                     os.path.join(workdir, repo, addon_id))
-    os.system('git add --all .')
-    os.system('git commit -m "[{id}] {version}"'.format(
-        id=addon_id,
-        version=addon_version))
-    os.system('git push --force --quiet origin {0}'.format(addon_id))
+    execute(['git', 'add', '--all', '.'])
+    execute(['git', 'commit', '-m', '"[{addon}] {version}"'.format(
+        addon=addon_id,
+        version=addon_version)
+             ])
+    execute(['git', 'push', '-f', 'origin', addon_id])
 
 
-def prepare_pull_reqest(zipaddon: ZippedAddon, repo: str, branch: str) -> None:
+def prepare_pull_request(zipaddon: ZippedAddon, repo: str, branch: str) -> None:
     workdir = settings.WORKDIR
     try:
         create_addon_directory(workdir, zipaddon)
         prepare_pr_branch(repo, branch, workdir, zipaddon.id, zipaddon.version)
+    except Exception:
+        logging.exception('Error while preparing pull request!')
     finally:
-        shutil.rmtree(os.path.join(workdir, repo), ignore_errors=True)
+        # shutil.rmtree(os.path.join(workdir, repo), ignore_errors=True)
         shutil.rmtree(os.path.join(workdir, zipaddon.id), ignore_errors=True)
+
+
+def main():
+    import sys
+    sys.path.append(settings.BASE_DIR)
+    repo = 'repo-scripts'
+    branch = 'krypton'
+    addon = 'plugin.video.example-2.2.0.zip'
+    with open(os.path.join(settings.BASE_DIR,
+                           'test_data', addon), 'rb') as fo:
+        prepare_pull_request(ZippedAddon(fo), repo, branch)
+
+
+if __name__ == '__main__':
+    main()
