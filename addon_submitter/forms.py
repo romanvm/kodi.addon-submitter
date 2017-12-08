d6008167de57bdf5b17a52ac248e0ebc2c939ea8
Roman Miroshnychenko <romanvm@yandex.ru>
2017-12-08 23:09:06 +0200

    Implement zip upload validation

--- addon_submitter/forms.py
+++ addon_submitter/forms.py
@@ -2,15 +2,36 @@
 # Created on: 08.12.2017
 # Author: Roman Miroshnychenko aka Roman V.M. (roman1972@gmail.com)
 
-from django.forms import ModelForm
+from zipfile import BadZipFile
+from django.forms import ModelForm, ValidationError
 from .models import PullRequest
+from .zip_file import ZippedAddon
 
 
 class PullRequestForm(ModelForm):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         for field in self.visible_fields():
             field.field.widget.attrs['class'] = 'form-control'
+
+    def clean(self) -> dict:
+        """
+        Validate an addon .zip file
+
+        :return: form cleaned data
+        """
+        cleaned_data = super().clean()
+        fo = cleaned_data.get('zipped_addon')
+        try:
+            ZippedAddon(fo)
+        except (BadZipFile, FileNotFoundError):
+            self.add_error('zipped_addon',
+                           ValidationError(
+                               'Bad zip archive or missing addon.xml!'
+                           ))
+        else:
+            fo.seek(0)
+        return cleaned_data
 
     class Meta:
         model = PullRequest
