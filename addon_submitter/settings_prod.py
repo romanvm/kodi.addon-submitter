aecb16e5afcb8b520a2a117488c3444fbfa33ce1
Roman Miroshnychenko <romanvm@yandex.ru>
2017-12-12 23:01:16 +0200

    Add email ssl/tls options to production settings

--- addon_submitter/settings_prod.py
+++ addon_submitter/settings_prod.py
@@ -29,6 +29,8 @@ try:
     EMAIL_PORT = os.environ['EMAIL_PORT']
     EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
     EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_USE_SSL=bool(os.environ.get('EMAIL_USE_SSL'))
+    EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS'))
     GH_TOKEN = os.environ['GH_TOKEN']
 except KeyError as ex:
     raise ImproperlyConfigured(
