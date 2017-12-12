cd1fef38c888bfc2eedf660bd079f9cd3125b431
Roman Miroshnychenko <romanvm@yandex.ru>
2017-12-12 13:16:01 +0200

    Remove runserver.sh script

--- runserver.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script is used to run addon_submitter application
-# in a production environment.
-
-SOCKET="unix:~/kodi.addon-submitter/addon-submitter.sock"
-
-cd ~/kodi.addon-submitter
-source settings.sh
-pipenv run gunicorn --workers 3 --bind $SOCKET addon_submitter.wsgi:application
