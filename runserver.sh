718caadc5d3ddb0a4014af67418908bb6798dc43
Roman Miroshnychenko <romanvm@yandex.ru>
2017-12-12 13:02:39 +0200

    Fix socket URL in runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,7 @@
 # This script is used to run addon_submitter application
 # in a production environment.
 
-SOCKET=http://unix:$HOME/kodi.addon-submitter/addon-submitter.sock
+SOCKET="unix:$HOME/kodi.addon-submitter/addon-submitter.sock"
 
 cd $HOME/kodi.addon-submitter
 source settings.sh
